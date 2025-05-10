dd2337430b9c2cd6709e416d26f7c91f1713ef69
caaarolb <carol.boaventura@bgcbrasil.com.br>
2025-05-09 22:28:02 -0300

    ✨ Banco atualizado✨

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://fastchef.onrender.com"
+    baseURL: "https://deunome-backend-1.onrender.com"
 })
 
 export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) => {
